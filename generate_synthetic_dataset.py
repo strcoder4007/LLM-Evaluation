f578982e2187eba071aea07b65c4b44f68939c76
Shubham <mynameshubham23@gmail.com>
2025-02-24 18:41:34 +0530

    improvements

--- generate_synthetic_dataset.py
+++ generate_synthetic_dataset.py
@@ -2,6 +2,5 @@ from deepeval.dataset import EvaluationDataset
 
 dataset = EvaluationDataset()
 dataset.generate_goldens_from_docs(
-    document_paths=['datasets/raw_data/doc.pdf'],
-    max_goldens_per_document=10
+    document_paths=['/home/tagbin/Projects/LLM-Evaluation/datasets/raw_data/1.PLCP_Summary_QA.txt']
 )
